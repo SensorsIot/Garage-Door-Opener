60e0a0f5ea49b2374d8c63ce7e43f51653e05c63
sensorsiot <andreas.spiess@arumba.com>
2021-11-09 02:23:52 +0100

    Update Garage_Door_Opener.ino

--- Garage_Door_Opener/Garage_Door_Opener.ino
+++ Garage_Door_Opener/Garage_Door_Opener.ino
@@ -1,8 +1,8 @@
 /* Creator Andreas Spiess
- 9.2.2016
-
-Clone of a Garage Door Opener using a AD9850 module
-Based on work of https://zissisprojects.wordpress.com/2014/02/10/all-digital-fm-modulation-w-arduino-ad9850/
+  9.2.2016
+  Revision 8.11.2021
+  Clone of a Garage Door Opener using a AD9850 module
+  Based on work of https://zissisprojects.wordpress.com/2014/02/10/all-digital-fm-modulation-w-arduino-ad9850/
 
 */
 
@@ -16,14 +16,14 @@ bool v[] = {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1,
 
 unsigned long entry;
 
-uint32_t tword;    //tuning word 
+uint32_t tword;    //tuning word
 byte W[5] = {0, 0, 0, 0, 0};
 
 
 void setup()
 {
-  DDRD  = B10000000;                         //define PORTD PD7 as output, PD7 is pin 7 (FU_UD pin)
-  PORTD = B00000000; 
+  DDRB  = B10000000;                         //define PORTB PD7 as output, PD7 is pin 7 (FU_UD pin)
+  PORTB = B00000000;
   SPI.setDataMode(SPI_MODE0);                // mode 0 seems to be the right one
   SPI.setClockDivider(SPI_CLOCK_DIV2);       // this is pretty fast
   SPI.setBitOrder(LSBFIRST);                 // AD9850 wants LSB first
@@ -32,7 +32,8 @@ void setup()
 }
 
 void loop() {
-  for (byte i = 0; i < 80; i++) {
+  /*
+    for (byte i = 0; i < 80; i++) {
     entry = micros();
     if (v[i])  tword = FREQ + 85900;       //calculating the high freqency tuning word for AD9850
     else  tword = FREQ - 85900;            //calculating the low freqency tuning word for AD9850
@@ -42,16 +43,32 @@ void loop() {
     W[3] = (byte) (tword >> 24);
     W[4] = 0;                              //phase is zero
     //start sending with spi interface
-    PORTD = B10000000;
-    PORTD = B00000000;                     //pulse FU_UD
+    PORTB = B10000000;
+    PORTB = B00000000;                     //pulse FU_UD
     for (int j = 0; j < 5; j++)
     {
       SPI.transfer(W[j]);                 //send the word
     }
-    PORTD = B10000000;
-    PORTD = B00000000;                    //pulse FU_UD
+    PORTB = B10000000;
+    PORTB = B00000000;                    //pulse FU_UD
     while (micros() - entry <500); // wait till 500 uS are gone
-  }
+    }
+  */
 
-  //delay(6);   // space between packets
+  tword = FREQ - 85900;            //calculating the low freqency tuning word for AD9850
+  W[0] = (byte) tword;
+  W[1] = (byte) (tword >> 8);
+  W[2] = (byte) (tword >> 16);           //convert it to bytes
+  W[3] = (byte) (tword >> 24);
+  W[4] = 0;                              //phase is zero
+  //start sending with spi interface
+  PORTB = B10000000;
+  PORTB = B00000000;                     //pulse FU_UD
+  for (int j = 0; j < 5; j++)
+  {
+    SPI.transfer(W[j]);                 //send the word
+  }
+  PORTB = B10000000;
+  PORTB = B00000000;                    //pulse FU_UD
+  while(1);
 }
